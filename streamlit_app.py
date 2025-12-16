b1c65ae6eb3b2366edb69eaa1f414528ef3ed6f9
Shuvam Panday <143728976+shuvam2909@users.noreply.github.com>
2025-12-16 18:07:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ session = cnx.session()
 my_dataframe = (
     session
     .table("smoothies.public.fruit_options")
-    .select(col('FRUIT_NAME'),col('SEARCH_ON'))
+    .select(col('FRUIT_NAME'),col('SEARCH_ON')))
 #st.dataframe(data=my_dataframe,use_container_width=True)
 #st.stop()
 
