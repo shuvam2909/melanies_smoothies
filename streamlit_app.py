da72d28e1063cb81b2bf7f414df73ae8277fa34b
Shuvam Panday <143728976+shuvam2909@users.noreply.github.com>
2025-12-16 17:42:51 +0530

    Add st.stop() to halt Streamlit app execution

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,6 +18,7 @@ my_dataframe = (
     .table("smoothies.public.fruit_options")
     .select(col('FRUIT_NAME')),col('SEARCH_ON'))
 st.dataframe(data=my_dataframe,use_container_width=True)
+st.stop()
 
 
 ingredients_list = st.multiselect(
