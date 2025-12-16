7f61b5b0b2fe2d84907683c72f095ea53a701b18
Shuvam Panday <143728976+shuvam2909@users.noreply.github.com>
2025-12-16 17:41:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ my_dataframe = (
     session
     .table("smoothies.public.fruit_options")
     .select(col('FRUIT_NAME')),col('SEARCH_ON'))
-    st.dataframe(data=my_dataframe,use_container_width=True
+st.dataframe(data=my_dataframe,use_container_width=True
 
 
 ingredients_list = st.multiselect(
